75686c330070b7d0931c5b678d6856a67b0eb857
Ali <ali.s.tag2000@gmail.com>
2024-08-10 10:17:40 +0330

    Add the current and parent directories to the system PATH.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import sys
+import os
 from PySide6.QtWidgets import QApplication
-from ui.main_window import MainWindow
-from models.soil_profile import SoilProfile
-from models.spt_data import SPTData
 
 if __name__ == "__main__":
+    # Step 1: Find the current directory
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    # Step 2: Add the current directory to the system PATH
+    sys.path.append(current_directory)
+    sys.path.append(parent_directory)
+
+    from ui.main_window import MainWindow
+    from models.soil_profile import SoilProfile
+    from models.spt_data import SPTData
+
     app = QApplication(sys.argv)
 
     soil_profile = SoilProfile()
